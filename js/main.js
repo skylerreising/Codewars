d79138b1e25a8a66ff306cad9988cf88de0f44d4
Skyler Reising <skyler.reising@gmail.com>
2023-07-28 13:56:48 -0500

    Comment out code

--- js/main.js
+++ js/main.js
@@ -140,14 +140,14 @@ SOLUTIONS*/
 
 // Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 
-function lovefunc(flower1, flower2){
-    if(flower1 % 2 === 0 && flower2 % 2 !== 0){
-        return true
-    }else if(flower2 % 2 === 0 && flower1 % 2 !== 0){
-        return true
-    }else {
-        return false
-    };
-}
-console.log(lovefunc(4,5))
-console.log(lovefunc(4,6))
+// function lovefunc(flower1, flower2){
+//     if(flower1 % 2 === 0 && flower2 % 2 !== 0){
+//         return true
+//     }else if(flower2 % 2 === 0 && flower1 % 2 !== 0){
+//         return true
+//     }else {
+//         return false
+//     };
+// }
+// console.log(lovefunc(4,5))
+// console.log(lovefunc(4,6))
