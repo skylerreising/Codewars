263cc9bfe6eb0251a6330632c53824c8921a993c
Skyler Reising <skyler.reising@gmail.com>
2023-07-29 11:12:39 -0500

    Codewars 7/29/23

--- js/main.js
+++ js/main.js
@@ -151,3 +151,19 @@ SOLUTIONS*/
 // }
 // console.log(lovefunc(4,5))
 // console.log(lovefunc(4,6))
+
+// Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example (Input -> Output):
+
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+function summation(num) {
+    let sum = 0
+    for(let i=1; i<=num; i++){
+        sum += i
+    }
+    return(sum) 
+  }
