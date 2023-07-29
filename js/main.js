2d7814aaacc8180c11b8d948a3b744782df18b6b
Skyler Reising <skyler.reising@gmail.com>
2023-07-29 14:10:38 -0500

    More Codewars 7/29/23

--- js/main.js
+++ js/main.js
@@ -160,10 +160,55 @@ SOLUTIONS*/
 // 2 -> 3 (1 + 2)
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
-function summation(num) {
-    let sum = 0
-    for(let i=1; i<=num; i++){
-        sum += i
-    }
-    return(sum) 
+// function summation(num) {
+//     let sum = 0
+//     for(let i=1; i<=num; i++){
+//         sum += i
+//     }
+//     return(sum) 
+//   }
+
+// function paperwork(n, m) {
+//   if(n<=0 || m<=0){
+//     return 0
+//   }else {
+//     return (n*m)
+//   }
+// }
+// console.log(paperwork(5,5))
+// console.log(paperwork(5,-5))
+// console.log(paperwork(-5,-5))
+// console.log(paperwork(-5,5))
+// console.log(paperwork(5,0))
+
+// DESCRIPTION:
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+function getGrade (s1, s2, s3) {
+    let ave = (s1+s2+s3)/3
+    if(ave<60){
+        return 'F'
+    }else if(ave>=60 && ave<70){
+        return 'D'
+    }else if(ave>=70 && ave<80){
+        return 'C'
+    }else if(ave>=80 && ave<90){
+        return 'B'
+    }else {
+        return 'A'
+    } 
   }
+  console.log(getGrade(100,100,100))
+  console.log(getGrade(89,89,89))
+  console.log(getGrade(79,79,79))
+  console.log(getGrade(69,69,69))
+  console.log(getGrade(59,59,59))
