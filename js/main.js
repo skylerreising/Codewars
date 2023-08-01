5cb18c5575690431313fcaa4f8243a270b57cee7
Skyler Reising <skyler.reising@gmail.com>
2023-07-31 22:10:10 -0500

    Codewars upload 7/31/23

--- js/main.js
+++ js/main.js
@@ -232,9 +232,19 @@ SOLUTIONS*/
 // "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
 // "8aaaaa dddd r     " -> "8aaaaaddddr"
 
-function noSpace(x){
-   x = x.replace(/\s+/g,'')
-   return x
-}
+// function noSpace(x){
+//    x = x.replace(/\s+/g,'')
+//    return x
+// }
 
-console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"))
+// console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"))
+
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+// Return true if yes, false otherwise :)
+
+function hero(bullets, dragons){
+    return dragons<=bullets/2 ? true:false
+    }
+console.log(hero(10,5))
+console.log(hero(7,4))
