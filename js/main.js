0191c0ce585459417d6b5e300b23d4549197f48a
Skyler Reising <skyler.reising@gmail.com>
2023-08-04 17:48:17 -0500

    Codewars Upload 8/4/23

--- js/main.js
+++ js/main.js
@@ -318,18 +318,42 @@ SOLUTIONS*/
 // "ATTGC" --> "TAACG"
 // "GTAT" --> "CATA"
 
-function DNAStrand(dna){
-    const complements = {
-        'A' : 'T',
-        'T' : 'A',
-        'C' : 'G',
-        'G' : 'C',
-    };
+// function DNAStrand(dna){
+//     const complements = {
+//         'A' : 'T',
+//         'T' : 'A',
+//         'C' : 'G',
+//         'G' : 'C',
+//     };
 
-    const newStrand = [...dna].map((symbol) => {
-        return complements.hasOwnProperty(symbol) ? complements[symbol] : symbol
-    }).join('');
-    return newStrand
-}
-  console.log(DNAStrand("ATTGC"))
-  console.log(DNAStrand("GTAT"))
+//     const newStrand = [...dna].map((symbol) => {
+//         return complements.hasOwnProperty(symbol) ? complements[symbol] : symbol
+//     }).join('');
+//     return newStrand
+// }
+//   console.log(DNAStrand("ATTGC"))
+//   console.log(DNAStrand("GTAT"))
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers){
+    let numArr = numbers.split(' ').map(Number)
+
+    let highNum = Math.max(...numArr)
+    let lowNum = Math.min(...numArr)
+
+    return `${highNum} ${lowNum}`
+  }
+
+console.log(highAndLow("1 2 3 4 5"));  // return "5 1"
+console.log(highAndLow("1 2 -3 4 5")); // return "5 -3"
+console.log(highAndLow("1 9 3 4 -5")); // return "9 -5"
