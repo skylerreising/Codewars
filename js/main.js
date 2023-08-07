7aabb824eeee3bce0f0b30e5fdde9939caf0b89e
Skyler Reising <skyler.reising@gmail.com>
2023-08-06 21:44:40 -0500

    Another Codewars Upload 8/6/23

--- js/main.js
+++ js/main.js
@@ -424,12 +424,57 @@ SOLUTIONS*/
 
 // Note: input will never be an empty string
 
-function fakeBin(x){
-    let stringToNum = x.split("").map((x) => +x)
+// function fakeBin(x){
+//     let stringToNum = x.split("").map((x) => +x)
     
-    let binString = stringToNum.map((x) => x<5 ? "0":"1").join("")
+//     let binString = stringToNum.map((x) => x<5 ? "0":"1").join("")
 
-    return binString
-}
+//     return binString
+// }
 
-console.log(fakeBin('45385593107843568'))
+// console.log(fakeBin('45385593107843568'))
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// function check(a, x) {
+//     let doesInclude = a.includes(x) ? true:false
+//     return doesInclude
+//   }
+
+//   console.log(check([66,101], 66))
+
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+// function smash (words) {
+//     let newSentence = words.join(" ").trim()
+//     return newSentence
+//  };
+
+//  console.log(smash(["hello", "amazing", "world"]))
+
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna) {
+    let DNA = dna.replaceAll("T","U")
+    return DNA
+  }
+
+  console.log(DNAtoRNA("GCAT"))
+  console.log(DNAtoRNA("UTTT"))
