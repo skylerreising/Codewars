c8b14256648c1b1da7e01fb026357f82999bda5d
Skyler Reising <skyler.reising@gmail.com>
2023-08-10 16:11:28 -0500

    Codewars Upload 8/10/23

--- js/main.js
+++ js/main.js
@@ -590,9 +590,22 @@ SOLUTIONS*/
 //     return sum = nums.reduce((a,b) => a+b)
 // }
 
-function sumMix(x){
-    let sum = 0
-    return sum = x.map((c) => Number(c)).reduce((a,b) => a+b)
-}
+// function sumMix(x){
+//     let sum = 0
+//     return sum = x.map((c) => Number(c)).reduce((a,b) => a+b)
+// }
 
-console.log(sumMix([9, 3, '7', '3']))
+// console.log(sumMix([9, 3, '7', '3']))
+
+// All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+// Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+// Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+function feast(beast, dish) {
+   return beast.charAt(0) == dish.charAt(0) && beast.charAt(beast.length-1) === dish.charAt(dish.length-1) ? true : false
+    }
+
+console.log(feast("great blue heron", "garlic naan"))
+console.log(feast("brown bear", "bear claw"))
