1f4175c457d88779501754a61418377f2cdbf715
Skyler Reising <skyler.reising@gmail.com>
2023-08-15 06:43:37 -0500

    Codewars Upload 8/15/23

--- js/main.js
+++ js/main.js
@@ -788,14 +788,38 @@ SOLUTIONS*/
 
 // Note: Empty arrays should return 0.
 
-function findAverage(array) {
-    // let sum = array.reduce((a,b) => a+b)
-    // let ave = sum/array.length
-    // return ave
-    return array.length === 0 ? 0 : array.reduce((a,b) => a+b, 0)/array.length
+// function findAverage(array) {
+//     // let sum = array.reduce((a,b) => a+b)
+//     // let ave = sum/array.length
+//     // return ave
+//     return array.length === 0 ? 0 : array.reduce((a,b) => a+b, 0)/array.length
+//   }
+
+//   console.log(findAverage([1,1,1]))
+//   console.log(findAverage([1,2,3]))
+//   console.log(findAverage([1,2,3,4]))
+//   console.log(findAverage([]))
+
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+    let positives = 0
+    let negatives = 0
+    let emptyArr = []
+    if(input === null || input.length === 0 ){
+        return emptyArr
+    }else {
+        input.map((x) => x>0 ? positives++ :negatives += x)
+    return [positives, negatives]
+    }
   }
 
-  console.log(findAverage([1,1,1]))
-  console.log(findAverage([1,2,3]))
-  console.log(findAverage([1,2,3,4]))
-  console.log(findAverage([]))
+  console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+  console.log(countPositivesSumNegatives([]))
