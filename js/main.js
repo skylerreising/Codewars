d302bee73080517dd2599bfab655d59fa172bc76
Skyler Reising <skyler.reising@gmail.com>
2023-08-16 06:06:09 -0500

    Codewars upload 8/16/23

--- js/main.js
+++ js/main.js
@@ -828,8 +828,29 @@ SOLUTIONS*/
 
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
-function grow(x){
-    return x.reduce((a,b) => a*b)
-}
+// function grow(x){
+//     return x.reduce((a,b) => a*b)
+// }
 
-console.log(grow([1, 2, 3, 4]))
+// console.log(grow([1, 2, 3, 4]))
+
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+
+// Examples
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+function countBy(x, n) {
+    let z = [];
+    for(let i=1; i<=n; i++){
+        z.push(i*x)
+    }
+    return z;
+  }
+
+  console.log(countBy(1,10))
+  console.log(countBy(2,5))
