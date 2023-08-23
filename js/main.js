2d27de763bd1ed9e08964f64afb2c8ab6463c7b5
Skyler Reising <skyler.reising@gmail.com>
2023-08-23 06:12:24 -0500

    Another Codewars Upload 8/23/23

--- js/main.js
+++ js/main.js
@@ -1288,21 +1288,39 @@ SOLUTIONS*/
 
 // In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 
-function well(x){
-    let goods = [0]
-    for(let i=0; i<x.length; i++){
-      if(x[i]==="good"){
-        goods++
-      }
-    }
-    if(1<=goods && goods<=2){
-      return "Publish!"
-    }else if(goods>2){
-      return "I smell a series!"
-    }else {
-      return "Fail!"
-    }
+// function well(x){
+//     let goods = [0]
+//     for(let i=0; i<x.length; i++){
+//       if(x[i]==="good"){
+//         goods++
+//       }
+//     }
+//     if(1<=goods && goods<=2){
+//       return "Publish!"
+//     }else if(goods>2){
+//       return "I smell a series!"
+//     }else {
+//       return "Fail!"
+//     }
+//   }
+//   console.log(well(['bad', 'bad', 'bad']))
+//   console.log(well(['good', 'bad', 'bad', 'bad', 'bad']))
+//   console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']))
+
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+function distinct(a) {
+    return Array.from(new Set(a));
   }
-  console.log(well(['bad', 'bad', 'bad']))
-  console.log(well(['good', 'bad', 'bad', 'bad', 'bad']))
-  console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']))
+
+  console.log(distinct([1]))
+  console.log(distinct([1,2]))
+  console.log(distinct([1,1,2]))
