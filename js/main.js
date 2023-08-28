515678bc95aa3414030dc392212a6360bca3e60b
Skyler Reising <skyler.reising@gmail.com>
2023-08-28 16:36:36 -0500

    Another Codewars 8/28/23

--- js/main.js
+++ js/main.js
@@ -1729,14 +1729,52 @@ SOLUTIONS*/
 // Example (Input --> Output)
 // "CodEWaRs" --> [0,3,4,6]
 
-var capitals = function (word) {
-	let capArray = []
-    for(let i=0; i<word.length; i++){
-        if(word[i]===word[i].toUpperCase()){
-            capArray.push(i)
+// var capitals = function (word) {
+// 	let capArray = []
+//     for(let i=0; i<word.length; i++){
+//         if(word[i]===word[i].toUpperCase()){
+//             capArray.push(i)
+//         }
+//     }
+//     return capArray
+// };
+
+// console.log(capitals("CodEWaRs"))
+
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+// Your task is to return the number of JavaScript developers coming from Europe.
+
+// For example, given the following list:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+// your function should return number 1.
+
+// If, there are no JavaScript developers from Europe then your function should return 0.
+
+// Notes:
+
+// The format of the strings will always be Europe and JavaScript.
+// All data will always be valid and uniform as in the example above.
+
+function countDevelopers(list) {
+    let count = 0
+    for(let i=0; i<list.length; i++){
+        if(list[i].continent==="Europe" && list[i].language==="JavaScript"){
+            count++
         }
     }
-    return capArray
-};
+    return count
+  }
 
-console.log(capitals("CodEWaRs"))
+  console.log(countDevelopers([
+      { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+      { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+      { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+      { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+    ]))
