a503aed71f379ebf753d82eed9828812fe19a669
Skyler Reising <skyler.reising@gmail.com>
2023-09-01 21:17:43 -0500

    Codewars 9/1/23

--- js/main.js
+++ js/main.js
@@ -1948,17 +1948,89 @@ SOLUTIONS*/
 // Task
 // Given a number, Find if it is Tidy or not .
 
-function tidyNumber(n){
-    let check = 0
-    let numArr = n.toString().split("").map((x)=>+x)
-    for(let i=0; i<numArr.length; i++){
-        if(numArr[i]>numArr[i+1]){
-            return false
-        }
-    }
-    return true
-  }
+// function tidyNumber(n){
+//     let check = 0
+//     let numArr = n.toString().split("").map((x)=>+x)
+//     for(let i=0; i<numArr.length; i++){
+//         if(numArr[i]>numArr[i+1]){
+//             return false
+//         }
+//     }
+//     return true
+//   }
   
-  console.log(tidyNumber(12))
-  console.log(tidyNumber(102))
-  console.log(tidyNumber(9672))
+//   console.log(tidyNumber(12))
+//   console.log(tidyNumber(102))
+//   console.log(tidyNumber(9672))
+
+// Once upon a time, on a way through the old wild mountainous west,…
+// … a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+
+// Going to one direction and coming back the opposite direction right away is a needless effort. Since this is the wild west, with dreadful weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
+
+// How I crossed a mountainous desert the smart way.
+// The directions given to the man are, for example, the following (depending on the language):
+
+// ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
+// or
+// { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+// or
+// [North, South, South, East, West, North, West]
+// You can immediately see that going "NORTH" and immediately "SOUTH" is not reasonable, better stay to the same place! So the task is to give to the man a simplified version of the plan. A better plan in this case is simply:
+
+// ["WEST"]
+// or
+// { "WEST" }
+// or
+// [West]
+// Other examples:
+// In ["NORTH", "SOUTH", "EAST", "WEST"], the direction "NORTH" + "SOUTH" is going north and coming back right away.
+
+// The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other, therefore, the final result is [] (nil in Clojure).
+
+// In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], "NORTH" and "SOUTH" are not directly opposite but they become directly opposite after the reduction of "EAST" and "WEST" so the whole path is reducible to ["WEST", "WEST"].
+
+// Task
+// Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+// function dirReduc(arr){
+//     let newArr = []
+//     let norSouSum = 0
+//     let eastWestSum = 0
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i]==="NORTH"){
+//             norSouSum++
+//         }else if(arr[i]==="SOUTH"){
+//             norSouSum--
+//         }else if(arr[i]==="EAST"){
+//             eastWestSum++
+//         }else if(arr[i]==="WEST"){
+//             eastWestSum--
+//         }
+//     }
+//     if(eastWestSum>0){
+//         newArr.push("EAST")
+//     }else if(eastWestSum<0){
+//         newArr.push("WEST")
+//     }else if(norSouSum<0){
+//         newArr.push("SOUTH")
+//     }else if(norSouSum>0){
+//         newArr.push("NORTH")
+//     }
+//     return newArr
+//   }
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+function last(x){
+    return x.split(" ")
+            .map((word) => ({ word, lastChar: word.slice(-1) })) // Create an array of objects with words and their last characters
+            .sort((a, b) => a.lastChar.localeCompare(b.lastChar))   // Sort by last character
+            .map(({ word }) => word);                             // Extract the sorted words
+  }
+  console.log(last('man i need a taxi up to ubud'))
+  console.log(last('what time are we climbing up the volcano'))
