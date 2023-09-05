ea6c43a5249818f627bd24890501e30a4def87d5
Skyler Reising <skyler.reising@gmail.com>
2023-09-04 19:36:52 -0500

    Codewars 9/4/23

--- js/main.js
+++ js/main.js
@@ -2115,12 +2115,33 @@ SOLUTIONS*/
 // ('*', 5, 5) --> 25
 // ('/', 49, 7) --> 7
 
-function basicOp(operation, value1, value2){
-    return eval(value1+operation+value2)
-}
+// function basicOp(operation, value1, value2){
+//     return eval(value1+operation+value2)
+// }
 
 
-console.log(basicOp('+', 4, 7))
-console.log(basicOp('-', 15, 18))
-console.log(basicOp('*', 5, 5))
-console.log(basicOp('/', 49, 7))
+// console.log(basicOp('+', 4, 7))
+// console.log(basicOp('-', 15, 18))
+// console.log(basicOp('*', 5, 5))
+// console.log(basicOp('/', 49, 7))
+
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year) {
+    return Math.ceil(year*.01)
+  }
+
+  console.log(century(1705))
+  console.log(century(1900))
+  console.log(century(1601))
+  console.log(century(2000))
+  console.log(century(89))
