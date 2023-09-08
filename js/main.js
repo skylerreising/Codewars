41d81608dc1f32c22587ba9e9bed727f177c53b6
Skyler Reising <skyler.reising@gmail.com>
2023-09-07 20:01:30 -0500

    Codewars 9/7/23

--- js/main.js
+++ js/main.js
@@ -2192,16 +2192,39 @@ SOLUTIONS*/
 
 // if bmi > 30 return "Obese"
 
-function bmi(weight, height) {
-    if(weight/height**2<=18.5){
-        return "Underweight"
-    }else if(weight/height**2<=25.0){
-        return "Normal"
-    }else if(weight/height**2<=30.0){
-        return "Overweight"
-    }else{
-        return "Obese"
-    }
-  }
+// function bmi(weight, height) {
+//     if(weight/height**2<=18.5){
+//         return "Underweight"
+//     }else if(weight/height**2<=25.0){
+//         return "Normal"
+//     }else if(weight/height**2<=30.0){
+//         return "Overweight"
+//     }else{
+//         return "Obese"
+//     }
+//   }
 
-  console.log(bmi(80, 1.80))
+//   console.log(bmi(80, 1.80))
+
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+// Examples (Input -> Output)
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+// Notes
+// You may consider that there will not be any empty arrays/vectors.
+
+var min = function(list){
+    
+    return Math.min(...list);
+}
+
+var max = function(list){
+    
+    return Math.max(...list);
+}
+
+console.log(min([4,6,2,1,9,63,-134,566]))
+console.log(max([4,6,2,1,9,63,-134,566]))
