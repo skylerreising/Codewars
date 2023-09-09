9ebb7d0774efea80307252911b36c2b240a35cf1
Skyler Reising <skyler.reising@gmail.com>
2023-09-08 21:14:34 -0500

    Last Codewars for the night 9/8/23

--- js/main.js
+++ js/main.js
@@ -2288,17 +2288,47 @@ SOLUTIONS*/
 
 // Write a code that gives out the total amount for different days(d).
 
-function rentalCarCost(d) {
-    let total = d*40
-    if(d>=7){
-        return total-50
-    }else if(d>=3){
-        return total-20
-    }else {
-        return total
-    }
-  }
+// function rentalCarCost(d) {
+//     let total = d*40
+//     if(d>=7){
+//         return total-50
+//     }else if(d>=3){
+//         return total-20
+//     }else {
+//         return total
+//     }
+//   }
 
-  console.log(rentalCarCost(2))
-  console.log(rentalCarCost(6))
-  console.log(rentalCarCost(10))
+//   console.log(rentalCarCost(2))
+//   console.log(rentalCarCost(6))
+//   console.log(rentalCarCost(10))
+
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findOdd(A) {
+    let counts = {}
+    for(let num of A){
+        counts[num] = counts[num] ? counts[num] + 1 : 1
+    }
+    for(let key in counts){
+        if(counts.hasOwnProperty(key)){
+            if(counts[key]%2!==0){
+                return +key
+            }
+        }
+    }
+}
+
+  console.log(findOdd([7]))
+  console.log(findOdd([0]))
+  console.log(findOdd([1,1,2]))
+  console.log(findOdd([0,1,0,1,0]))
