3ce3e4fafeda9f3d5a6a925d1820c1ec254b2f2b
Skyler Reising <skyler.reising@gmail.com>
2023-09-08 19:55:31 -0500

    Codewars again 9/8/23

--- js/main.js
+++ js/main.js
@@ -2246,12 +2246,36 @@ SOLUTIONS*/
 
 // Example : n=5 --> [5,4,3,2,1]
 
-const reverseSeq = n => {
-    let newArr = []
-    for(let i=n; i>=1; i--){
-      newArr.push(i)
-    }
-    return newArr
-  };
+// const reverseSeq = n => {
+//     let newArr = []
+//     for(let i=n; i>=1; i--){
+//       newArr.push(i)
+//     }
+//     return newArr
+//   };
   
-  console.log(reverseSeq(5))
+//   console.log(reverseSeq(5))
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+// Constraint:
+
+// 1 <= month <= 12
+
+const quarterOf = (month) => {
+    if(month<=3){
+        return 1
+    }else if(month<=6){
+        return 2
+    }else if(month<=9){
+        return 3
+    }else {
+        return 4
+    }
+  }
+
+  console.log(quarterOf(3))
+  console.log(quarterOf(8))
+  console.log(quarterOf(11))
