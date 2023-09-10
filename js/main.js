e52de7e43f1a127614de580a40fa6dd303db49ee
Skyler Reising <skyler.reising@gmail.com>
2023-09-10 09:21:00 -0500

    Another codewars 9/10/23

--- js/main.js
+++ js/main.js
@@ -2393,8 +2393,71 @@ SOLUTIONS*/
 
 // Note: only positive integers will be tested.
 
-function otherAngle(a, b) {
-    return 180-a-b
-  }
+// function otherAngle(a, b) {
+//     return 180-a-b
+//   }
 
-  console.log(otherAngle(30,60))
+//   console.log(otherAngle(30,60))
+
+// A square of squares
+// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+
+// var isSquare = function(n){
+//     return Number.isInteger(Math.sqrt(n))
+//   }
+
+//   console.log(isSquare(-1))
+//   console.log(isSquare(0))
+//   console.log(isSquare(3))
+//   console.log(isSquare(4))
+//   console.log(isSquare(25))
+//   console.log(isSquare(26))
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+  if(b.length<1){
+        return a
+      }else {
+      for(let i=0; i<b.length; i++){
+        for(let j=0; j<a.length; j++){
+            if(b[i]===a[j]){
+                a.splice(j,1)
+                j--
+            }
+        }
+      }
+      return a
+    }
+}
+
+console.log(arrayDiff([1,2], [1]))
+console.log(arrayDiff([1,2,2], [1]))
+console.log(arrayDiff([1,2,2], [2]))
+console.log(arrayDiff([1,2,2], []))
+console.log(arrayDiff([], [1,2]))
+console.log(arrayDiff([1,2,3], [1,2]))
