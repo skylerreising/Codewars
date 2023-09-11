391748f6fbebbbd54a60dde2b230a960678ca3fd
Skyler Reising <skyler.reising@gmail.com>
2023-09-11 15:33:29 -0500

    Another codewars 9/11/23

--- js/main.js
+++ js/main.js
@@ -2471,9 +2471,77 @@ SOLUTIONS*/
 
 // Input: 123456789 Output: 987654321
 
-function descendingOrder(n){
-    return +n.toString().split("").sort().reverse().join("")
+// function descendingOrder(n){
+//     return +n.toString().split("").sort().reverse().join("")
+//   }
+//   console.log(descendingOrder(42145))
+//   console.log(descendingOrder(145263))
+//   console.log(descendingOrder(123456789))
+
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
+
+// function getMiddle(s){
+//   if(s.length % 2===1){
+//     return s.charAt(s.length/2)
+//   }else{
+//     let characterBefore = s.length/2-1
+//     return `${s.charAt(characterBefore)}${s.charAt(s.length/2)}`
+//   }
+// }
+
+// console.log(getMiddle("test"))
+// console.log(getMiddle("testing"))
+// console.log(getMiddle("middle"))
+// console.log(getMiddle("A"))
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+// function filter_list(l){
+//   return l.filter(x => x===Number(x))
+// }
+
+// console.log(filter_list([1,2,'a','b']))
+// console.log(filter_list([1,'a','b',0,15]))
+// console.log(filter_list([1,2,'aasf','1','123',123]))
+
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+// isIsogram "Dermatoglyphics" = true
+// isIsogram "moose" = false
+// isIsogram "aba" = false
+
+function isIsogram(str){
+    const newStr = str.toLowerCase()
+    const noRepeat = Array.from(new Set (str.toLowerCase().split("")))
+    return newStr === noRepeat.join("")
   }
-  console.log(descendingOrder(42145))
-  console.log(descendingOrder(145263))
-  console.log(descendingOrder(123456789))
+  
+  console.log(isIsogram("Dermatoglyphics"))
+  console.log(isIsogram("moOse"))
+  console.log(isIsogram("aba"))
