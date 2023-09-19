9f4d003a36c5ee96a5810abe57de4e949e983545
Skyler Reising <skyler.reising@gmail.com>
2023-09-19 16:23:51 -0500

    My first 5kyu on codewars! 9/19/23

--- js/main.js
+++ js/main.js
@@ -3473,19 +3473,44 @@ input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
  */
 
-function openOrSenior(data){
-    //greater than or equal to 55 years old & handicap greater than 7
-    let seniorOrOpen = []
-    let seniorAge = 55
-    let handicapLimit = 7
-    for(let i=0; i<data.length; i++){
-        if(data[i][0]>=seniorAge && data[i][1]>handicapLimit){
-            seniorOrOpen.push("Senior")
-        }else{
-            seniorOrOpen.push("Open")
+// function openOrSenior(data){
+//     //greater than or equal to 55 years old & handicap greater than 7
+//     let seniorOrOpen = []
+//     let seniorAge = 55
+//     let handicapLimit = 7
+//     for(let i=0; i<data.length; i++){
+//         if(data[i][0]>=seniorAge && data[i][1]>handicapLimit){
+//             seniorOrOpen.push("Senior")
+//         }else{
+//             seniorOrOpen.push("Open")
+//         }
+//     }
+//     return seniorOrOpen
+//   }
+
+//   console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
+
+/**
+ * Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+ */
+
+function moveZeros(arr) {
+    let zeroCount = 0
+    //count zeros
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]===0){
+            zeroCount++
         }
     }
-    return seniorOrOpen
+    //filter zeros
+    let filteredArray = arr.filter(x => x!==0)
+    //add zeros to the end
+    for(let i=0; i<zeroCount; i++){
+        filteredArray.push(0)
+    }
+    return filteredArray
   }
 
-  console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
+  console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
