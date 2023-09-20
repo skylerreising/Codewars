b6b04ccf4d0fe9d2c61fd19fe6d39f9d58642a3d
Skyler Reising <skyler.reising@gmail.com>
2023-09-20 05:37:29 -0500

    Another codewars this morning 9/20/23

--- js/main.js
+++ js/main.js
@@ -3644,10 +3644,40 @@ Examples:
 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
  */
 
-function isDivisible(n, x, y) {
-    return n%x===0 && n%y===0 ? true:false
-}
+// function isDivisible(n, x, y) {
+//     return n%x===0 && n%y===0 ? true:false
+// }
 
-console.log(isDivisible(3,3,4))//false
-console.log(isDivisible(12,3,4))//true
-console.log(isDivisible(8,3,4))//false
+// console.log(isDivisible(3,3,4))//false
+// console.log(isDivisible(12,3,4))//true
+// console.log(isDivisible(8,3,4))//false
+
+/**
+ * There is a bus moving in the city which takes and drops some people at each bus stop.
+
+You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+Take a look on the test cases.
+
+Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+ */
+
+var number = function(busStops){
+    //loop through the array and change the count of how many people are on and off the bus then return the sum
+    let onBus = 0
+    let offBus = 0
+    for(let i=0; i<busStops.length; i++){
+        onBus += busStops[i][0]
+        offBus -= busStops[i][1]
+    }
+    return onBus + offBus
+  }
+
+  console.log(number([[10,0],[3,5],[5,8]]))
+  console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]))
+  console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]))
+  console.log(number([[0,0]]))
