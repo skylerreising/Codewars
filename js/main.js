571ecddaab256ddeafec79f9e5d4747512b32dee
Skyler Reising <skyler.reising@gmail.com>
2023-09-20 05:23:15 -0500

    Codewars 9/20/23

--- js/main.js
+++ js/main.js
@@ -3585,26 +3585,51 @@ nb_year(1500000, 2.5, 10000, 2000000) -> 10
 // Note:
 // If you are given an array with multiple answers, return the lowest correct index.
 
-function findEvenIndex(arr){
-  //Loop through the array and on each iteration, check to see if the sum of the previous numbers is equal to the sum of the following numbers
-  //check for an empty array
-  if(arr.length === 0){
+// function findEvenIndex(arr){
+//   //Loop through the array and on each iteration, check to see if the sum of the previous numbers is equal to the sum of the following numbers
+//   //check for an empty array
+//   if(arr.length === 0){
+//     return -1
+//   }else{
+//     let leftSum = 0
+//     let rightSum = arr.reduce((a,b) => a+b, 0)
+//     for(let i=0; i<arr.length; i++){
+//         rightSum -= arr[i]
+        
+//         if(leftSum === rightSum){
+//           return i
+//         }
+//       leftSum += arr[i]
+//     }
+//     return -1
+//   }
+// }
+
+// console.log(findEvenIndex([1,2,3,4,3,2,1]))
+// console.log(findEvenIndex([]))
+// console.log(findEvenIndex([1,2,3,4,5,6]))
+
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+// Examples:(Input --> Output)
+
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  if(!Number.isInteger(Math.sqrt(sq))){
     return -1
   }else{
-    let leftSum = 0
-    let rightSum = arr.reduce((a,b) => a+b, 0)
-    for(let i=0; i<arr.length; i++){
-        rightSum -= arr[i]
-        
-        if(leftSum === rightSum){
-          return i
-        }
-      leftSum += arr[i]
-    }
-    return -1
+    return (Math.sqrt(sq)+1)*(Math.sqrt(sq)+1)
   }
 }
 
-console.log(findEvenIndex([1,2,3,4,3,2,1]))
-console.log(findEvenIndex([]))
-console.log(findEvenIndex([1,2,3,4,5,6]))
+console.log(findNextSquare(121))
+console.log(findNextSquare(625))
+console.log(findNextSquare(114))
