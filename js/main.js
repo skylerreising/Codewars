49a8069ee34777bd4c5c400719bd248bb9719df9
Skyler Reising <skyler.reising@gmail.com>
2023-09-19 20:54:34 -0500

    Last codewars of the night 9/19/23

--- js/main.js
+++ js/main.js
@@ -3496,21 +3496,115 @@ output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
  */
 
-function moveZeros(arr) {
-    let zeroCount = 0
-    //count zeros
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]===0){
-            zeroCount++
-        }
-    }
-    //filter zeros
-    let filteredArray = arr.filter(x => x!==0)
-    //add zeros to the end
-    for(let i=0; i<zeroCount; i++){
-        filteredArray.push(0)
-    }
-    return filteredArray
-  }
+// function moveZeros(arr) {
+//     let zeroCount = 0
+//     //count zeros
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i]===0){
+//             zeroCount++
+//         }
+//     }
+//     //filter zeros
+//     let filteredArray = arr.filter(x => x!==0)
+//     //add zeros to the end
+//     for(let i=0; i<zeroCount; i++){
+//         filteredArray.push(0)
+//     }
+//     return filteredArray
+//   }
 
-  console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
+//   console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
+
+/**
+ * In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+At the end of the first year there will be: 
+1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+At the end of the 2nd year there will be: 
+1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+At the end of the 3rd year there will be:
+1141 + 1141 * 0.02 + 50 => 1213
+
+It will need 3 entire years.
+More generally given parameters:
+
+p0, percent, aug (inhabitants coming or leaving each year), p (population to equal or surpass)
+
+the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+Examples:
+nb_year(1500, 5, 100, 5000) -> 15
+nb_year(1500000, 2.5, 10000, 2000000) -> 10
+ */
+
+// function nbYear(p0, percent, aug, p) {
+//     let years = 0
+//     let population = p0
+//     const percentage = percent*.01
+//     while(population<p){
+//         population = Math.trunc(population + population * percentage + aug)
+//         years++
+//     }
+//     return years
+// }
+
+// console.log(nbYear(1500, 5, 100, 5000))
+// console.log(nbYear(1500000, 2.5, 10000, 2000000))
+// console.log(nbYear(1500000, 0.25, 1000, 2000000))
+
+// You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+// For example:
+
+// Let's say you are given the array {1,2,3,4,3,2,1}:
+// Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+// Let's look at another one.
+// You are given the array {1,100,50,-51,1,1}:
+// Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+// Last one:
+// You are given the array {20,10,-80,10,10,15,35}
+// At index 0 the left side is {}
+// The right side is {10,-80,10,10,15,35}
+// They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+// Index 0 is the place where the left side and right side are equal.
+
+// Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
+
+// Input:
+// An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+
+// Output:
+// The lowest index N where the side to the left of N is equal to the side to the right of N. If you do not find an index that fits these rules, then you will return -1.
+
+// Note:
+// If you are given an array with multiple answers, return the lowest correct index.
+
+function findEvenIndex(arr){
+  //Loop through the array and on each iteration, check to see if the sum of the previous numbers is equal to the sum of the following numbers
+  //check for an empty array
+  if(arr.length === 0){
+    return -1
+  }else{
+    let leftSum = 0
+    let rightSum = arr.reduce((a,b) => a+b, 0)
+    for(let i=0; i<arr.length; i++){
+        rightSum -= arr[i]
+        
+        if(leftSum === rightSum){
+          return i
+        }
+      leftSum += arr[i]
+    }
+    return -1
+  }
+}
+
+console.log(findEvenIndex([1,2,3,4,3,2,1]))
+console.log(findEvenIndex([]))
+console.log(findEvenIndex([1,2,3,4,5,6]))
