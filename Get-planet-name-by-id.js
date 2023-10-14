6eb983edda9d1dd4aa831256e705f6c298106b64
Skyler Reising <skyler.reising@gmail.com>
2023-10-14 07:43:54 -0500

    Codewars 10/14/23

---
+++ Get-planet-name-by-id.js
@@ -0,0 +1,49 @@
+/*
+Get Planet Name By ID
+The function is not returning the correct values. Can you figure out why?
+
+Example (Input --> Output ):
+
+3 --> "Earth"
+*/
+
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break;
+    case 2:
+      name = 'Venus'
+      break;
+    case 3:
+      name = 'Earth'
+      break;
+    case 4:
+      name = 'Mars'
+      break;
+    case 5:
+      name = 'Jupiter'
+      break;
+    case 6:
+      name = 'Saturn'
+      break;
+    case 7:
+      name = 'Uranus'
+      break;
+    case 8:
+      name = 'Neptune'
+      break;
+  }
+  
+  return name;
+}
+
+console.log(getPlanetName(1))
+console.log(getPlanetName(2))
+console.log(getPlanetName(3))
+console.log(getPlanetName(4))
+console.log(getPlanetName(5))
+console.log(getPlanetName(6))
+console.log(getPlanetName(7))
+console.log(getPlanetName(8))
