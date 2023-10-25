d3e0ff2ebdeaa9ded955eaf03b6bdb1bda1c873b
Skyler Reising <skyler.reising@gmail.com>
2023-10-25 16:57:24 -0500

    Codewars 10/25/23

---
+++ Grasshopper-Summation.cs
@@ -0,0 +1,28 @@
+/*
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example (Input -> Output):
+
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+*/
+
+using System;
+
+public static class Kata 
+{
+    public static int summation(int num)
+    {
+        int sum = 0;
+
+        for(int i=1; i<=num; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
+
+Console.WriteLine(Kata.summation(3));
+Console.WriteLine(Kata.summation(8));
