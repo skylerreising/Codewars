f864f98a2cde2c63228f274ddc781929e32d8f36
Skyler Reising <skyler.reising@gmail.com>
2023-11-04 15:31:56 -0500

    Codewars 11/4/23

---
+++ Is-it-a-palindrome.js
@@ -0,0 +1,12 @@
+/*
+Is it a palindrome
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+*/
+function isPalindrome(x) {
+  return x.toLowerCase() === x.split("").reverse().join("").toLowerCase();
+}
+console.log(isPalindrome("madam"))
+console.log(isPalindrome("Racecar"))
+console.log(isPalindrome("skyler"))
