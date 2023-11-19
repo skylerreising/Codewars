4d02cabfb627825e2b83fab2d56533cd27b6277d
Skyler Reising <skyler.reising@gmail.com>
2023-11-19 17:04:11 -0600

    Codewars 11/19/23

---
+++ Squaring-an-argument.js
@@ -0,0 +1,8 @@
+/*
+Function 2 - squaring an argument
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+function square(n){
+  return Math.pow(n,2);
+}
+console.log(square(3))
