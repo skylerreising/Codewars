da6e9e7f753dc8275bffd960f32bf0de05c49f1e
Skyler Reising <skyler.reising@gmail.com>
2023-12-10 08:32:42 -0600

    Codewars 12/10/23 Remove String Spaces C#

--- app/Program.cs
+++ app/Program.cs
@@ -15,48 +15,77 @@ Method calling example:
 
 string[] result = FizzBuzz.GetFizzBuzzArray(3); // => [ "1", "2", "Fizz" ]
 */
+// using System;
+// using System.Collections.Generic;
+
+// public class FizzBuzz
+// {
+// 	public static string[] GetFizzBuzzArray(int n)
+// 	{
+//     string[] result = new string[n];
+//     for(int i=0; i<=n-1; i++)
+//     {
+//         int num = i+1;
+
+//         if(num % 3 == 0 && num % 5 == 0)
+//         {
+//             result[i] = "FizzBuzz";
+//         }
+//         else if(num % 3 == 0)
+//         {
+//             result[i] = "Fizz";
+//         }
+//         else if(num % 5 == 0)
+//         {
+//             result[i] = "Buzz";
+//         }
+//         else
+//         {
+//             result[i] = num.ToString();
+//         }
+//     }
+//     return result;
+// 	}
+
+//     public static void Main(string[] args)
+//     {
+//         int n = 100;
+//         string[] fizzBuzzArray = GetFizzBuzzArray(n);
+
+//         foreach(string element in fizzBuzzArray)
+//         {
+//             Console.WriteLine(element);
+//         }
+//     }
+// }
+
+/*
+Write a function that removes the spaces from the string, then return the resultant string.
+
+Examples:
+
+Input -> Output
+"8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+"8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+"8aaaaa dddd r     " -> "8aaaaaddddr"
+*/
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
-public class FizzBuzz
+namespace Solution 
 {
-	public static string[] GetFizzBuzzArray(int n)
-	{
-    string[] result = new string[n];
-    for(int i=0; i<=n-1; i++)
+  public static class SpacesRemover
+  {
+    public static string NoSpace(string input)
     {
-        int num = i+1;
-
-        if(num % 3 == 0 && num % 5 == 0)
-        {
-            result[i] = "FizzBuzz";
-        }
-        else if(num % 3 == 0)
-        {
-            result[i] = "Fizz";
-        }
-        else if(num % 5 == 0)
-        {
-            result[i] = "Buzz";
-        }
-        else
-        {
-            result[i] = num.ToString();
-        }
-    }
-    return result;
-	}
-
-    public static void Main(string[] args)
+        return new string(input.Where(c => !char.IsWhiteSpace(c)).ToArray());
+    }  
+     public static void Main(string[] args)
     {
-        int n = 100;
-        string[] fizzBuzzArray = GetFizzBuzzArray(n);
-
-        foreach(string element in fizzBuzzArray)
-        {
-            Console.WriteLine(element);
-        }
+        string input = "8 j 8   mBliB8g  imjB8B8  jl  B";
+        string newString = NoSpace(input);
+        Console.WriteLine(newString);
     }
+  }
 }
-
 
