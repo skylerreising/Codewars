938feca40d209bfdfb91354a8d480f479ec7699e
Skyler Reising <skyler.reising@gmail.com>
2023-12-27 19:25:27 -0600

    Another codewars 12/27/23

---
+++ Get-Char-from-ASCII-Value.js
@@ -0,0 +1,16 @@
+/**
+ * Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+ */
+
+function getChar(c){
+    return String.fromCharCode(c);
+  }
+console.log(getChar(55))
+console.log(getChar(56))
+console.log(getChar(57))
