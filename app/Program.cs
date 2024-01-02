14af97552e3e36eb16b88477d3897e1f5f3c979d
Skyler Reising <skyler.reising@gmail.com>
2024-01-01 20:25:22 -0600

    Another codewars 1/1/24

--- app/Program.cs
+++ app/Program.cs
@@ -328,28 +328,59 @@ Examples(Operator, value1, value2) --> output
 ('/', 49, 7) --> 7
 */
 
-namespace Solution
-{
-  public static class Program
-  {
-    public static double BasicOp(char operation, double value1, double value2)
-    {
-      switch (operation)
-      {
-        case '+' : return value1 + value2;
-        case '-' : return value1 - value2;
-        case '*' : return value1 * value2;
-        case '/' : return value1 / value2;
-        default: throw new ArgumentException("Invalid operation");
-      }
-    }
+// namespace Solution
+// {
+//   public static class Program
+//   {
+//     public static double BasicOp(char operation, double value1, double value2)
+//     {
+//       switch (operation)
+//       {
+//         case '+' : return value1 + value2;
+//         case '-' : return value1 - value2;
+//         case '*' : return value1 * value2;
+//         case '/' : return value1 / value2;
+//         default: throw new ArgumentException("Invalid operation");
+//       }
+//     }
 
-    public static void Main(string[] args)
-    {
-      Console.WriteLine(BasicOp('+', 4, 7));
-      Console.WriteLine(BasicOp('-', 15, 18));
-      Console.WriteLine(BasicOp('*', 5, 5));
-      Console.WriteLine(BasicOp('/', 49, 7));
-    }
+//     public static void Main(string[] args)
+//     {
+//       Console.WriteLine(BasicOp('+', 4, 7));
+//       Console.WriteLine(BasicOp('-', 15, 18));
+//       Console.WriteLine(BasicOp('*', 5, 5));
+//       Console.WriteLine(BasicOp('/', 49, 7));
+//     }
+//   }
+// }
+
+/*
+Keep Hydrated!
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+For example:
+
+time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres = 5
+*/
+using System;
+
+public class Kata
+{
+  public static int Litres(double time)
+  {
+    return (int)Math.Floor(time * .5);
+  }
+  public static void Main(string[] args)
+  {
+    Console.WriteLine(Litres(1.4));
+    Console.WriteLine(Litres(12.3));
   }
 }
