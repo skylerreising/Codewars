5edfaf06acd7808377c934b3e366b652c6b4ee29
Skyler Reising <skyler.reising@gmail.com>
2024-05-11 18:42:48 -0500

    Stop Spinning My Words TypeScript

---
+++ Stop-gninnipS-My-sdroW!.ts
@@ -0,0 +1,22 @@
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+"Hey fellow warriors"  --> "Hey wollef sroirraw" 
+"This is a test        --> "This is a test" 
+"This is another test" --> "This is rehtona test"
+*/
+
+export function spinWords(words: string): string {
+    // split into array of words
+    let stringArray: string[] = words.split(" ");
+    let newStringArray: string[];
+
+    // if length of each word is 5 or more, reverse it
+    newStringArray = stringArray.map(x => x.length >= 5 ? x.split("").reverse().join("") : x);
+
+    return newStringArray.join(" ");
+}
+
+console.log(spinWords("Hey fellow warriors"));
