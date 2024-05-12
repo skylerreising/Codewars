62f4c4f1a45269f025e0343543ee87502ede9bea
Skyler Reising <skyler.reising@gmail.com>
2024-05-12 18:33:04 -0500

    Greed is good in C#

--- app/Program.cs
+++ app/Program.cs
@@ -1312,25 +1312,153 @@ Examples
 26  =>  false
 */
 
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+// public class Kata
+// {
+//   public static bool IsSquare(int n)
+//   {
+//     double squareRoot = Math.Sqrt(n);
+
+//     return ((int)squareRoot)*((int)squareRoot) == n;
+//   }
+
+//   public static void Main(string[] args)
+//   {
+//     Console.WriteLine(IsSquare(0));
+//     Console.WriteLine(IsSquare(3));
+//     Console.WriteLine(IsSquare(4));
+//     Console.WriteLine(IsSquare(25));
+//     Console.WriteLine(IsSquare(26));
+//   }
+// }
+
+/*
+Greed is Good
+
+Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+
+ Three 1's => 1000 points
+ Three 6's =>  600 points
+ Three 5's =>  500 points
+ Three 4's =>  400 points
+ Three 3's =>  300 points
+ Three 2's =>  200 points
+ One   1   =>  100 points
+ One   5   =>   50 point
+A single die can only be counted once in each roll. For example, a given "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+
+Example scoring
+
+ Throw       Score
+ ---------   ------------------
+ 5 1 3 4 1   250:  50 (for the 5) + 2 * 100 (for the 1s)
+ 1 1 1 3 1   1100: 1000 (for three 1s) + 100 (for the other 1)
+ 2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
+In some languages, it is possible to mutate the input to the function. This is something that you should never do. If you mutate the input, you will not be able to pass all the tests.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
-public class Kata
+public static class Kata
 {
-  public static bool IsSquare(int n)
-  {
-    double squareRoot = Math.Sqrt(n);
+  public static int Score(int[] dice) {
+    // count each number throw
+    int count = 0;
+    int points = 0;
 
-    return ((int)squareRoot)*((int)squareRoot) == n;
+    for(int i = 1; i <= 6; i++)
+    {
+      // count each number
+      foreach(int d in dice)
+      {
+        if(d == i)
+        {
+          count++;
+        }
+      }
+
+      // apply rules for that number
+      if(i == 1)
+      {
+        // 1 is worth 100 as single, 1000 as triple
+        if(count >= 3)
+        {
+          points += 1000;
+          count -= 3;
+        }
+        for(int j=1; j <= count; j++)
+        {
+          points += 100;
+        }
+        count = 0;
+      }
+
+      if(i == 2)
+      {
+        // 2 is worth 200 as a triple
+        if(count >= 3)
+        {
+          points += 200;
+        }
+        count = 0;
+      }
+
+      if(i == 3)
+      {
+        // 3 is worth 300 as a triple
+        if(count >= 3)
+        {
+          points += 300;
+        }
+        count = 0;
+      }
+
+      if(i == 4)
+      {
+        // 4 is worth 400 as a triple
+        if(count >= 3)
+        {
+          points += 400;
+        }
+        count = 0;
+      }
+
+      if(i == 5)
+      {
+        //5 is worth 50 as single, 500 as triple
+        if(count >= 3)
+        {
+          points += 500;
+          count -= 3;
+        }
+        for(int j=1; j <= count; j++)
+        {
+          points += 50;
+        }
+        count = 0;
+      }
+
+      if(i == 6)
+      {
+        // 6 is worth 600 as a triple
+        if(count >= 3)
+        {
+          points += 600;
+        }
+        count = 0;
+      }
+    }
+    return points;
   }
 
   public static void Main(string[] args)
   {
-    Console.WriteLine(IsSquare(0));
-    Console.WriteLine(IsSquare(3));
-    Console.WriteLine(IsSquare(4));
-    Console.WriteLine(IsSquare(25));
-    Console.WriteLine(IsSquare(26));
+    Console.WriteLine(Score(new int[] {2, 3, 4, 6, 2}));
+    Console.WriteLine(Score(new int[] {4, 4, 4, 3, 3}));
+    Console.WriteLine(Score(new int[] {2, 4, 4, 5, 4}));
   }
 }
