c4b8967869f3ed227741b980d3db2bd0f766c40f
Skyler Reising <skyler.reising@gmail.com>
2024-06-02 13:31:32 -0500

    Added description

--- Find-the-odd-int.ts
+++ Find-the-odd-int.ts
@@ -1,3 +1,18 @@
+/*
+Find the odd int
+
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+*/
+
 export const findOdd = (xs: number[]): number => {
     return xs[xs.map(x => x = ([...xs].filter(y => x === y)).length)
     .findIndex(x => x % 2 === 1)];
