0340bb61f9b5f72ea4f4782a94fe82f272d73d57
Skyler Reising <skyler.reising@gmail.com>
2024-06-16 12:32:48 -0500

    Get the Middle Character in C#

--- app/Program.cs
+++ app/Program.cs
@@ -1675,32 +1675,80 @@ Additionally, if the number is negative, return 0.
 
 Note: If the number is a multiple of both 3 and 5, only count it once.
 */
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+// public static class Kata
+// {
+//   public static int Solution(int value)
+//   {
+//     if (value < 0) return 0;
+
+//     // return sum of array or list of all nums divisible by 3 or 5 below value
+//     List<int> list = new List<int>();
+
+//     for(int i = 1; i < value; i++)
+//     {
+//       if(i % 3 == 0 || i % 5 == 0)
+//       {
+//         list.Add(i);
+//       }
+//     }
+
+//     return list.Sum();
+//   }
+
+//   public static void Main(string[] args)
+//   {
+//     Console.WriteLine(Solution(10));
+//   }
+// }
+
+/*
+Get the Middle Character
+
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+#Output
+
+The middle character(s) of the word represented as a string.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
-public static class Kata
+public class Kata
 {
-  public static int Solution(int value)
+  public static string GetMiddle(string s)
   {
-    if (value < 0) return 0;
-
-    // return sum of array or list of all nums divisible by 3 or 5 below value
-    List<int> list = new List<int>();
-
-    for(int i = 1; i < value; i++)
-    {
-      if(i % 3 == 0 || i % 5 == 0)
-      {
-        list.Add(i);
-      }
-    }
-
-    return list.Sum();
+    double halfSLength = s.Length / 2;
+    return ((s.Length) % 2 == 1) ? 
+      s.ToCharArray()[(int)Math.Round(halfSLength, MidpointRounding.ToEven)].ToString():
+      s.ToCharArray()[(int)Math.Round(halfSLength, MidpointRounding.ToZero) - 1].ToString() +
+      s.ToCharArray()[(int)Math.Round(halfSLength, MidpointRounding.ToEven)].ToString();
   }
 
   public static void Main(string[] args)
   {
-    Console.WriteLine(Solution(10));
+    Console.WriteLine(GetMiddle("test"));
+    Console.WriteLine(GetMiddle("testing"));
+    Console.WriteLine(GetMiddle("middle"));
+
   }
 }
