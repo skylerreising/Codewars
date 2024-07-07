f213f7fdc9c38d57c95593e26c9d96661a17abf8
Skyler Reising <skyler.reising@gmail.com>
2024-07-07 10:44:57 -0500

    Find the Parity Outlier in C#

--- app/Program.cs
+++ app/Program.cs
@@ -1765,31 +1765,76 @@ Example: (Input --> Output)
 "moOse" --> false (ignore letter case)
 */
 
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+
+// public class Kata
+// {
+//   public static bool IsIsogram(string str) 
+//   {
+//     str = str.ToLower();
+//     foreach (char c in str)
+//     {
+//       if (str.IndexOf(c) != str.LastIndexOf(c))
+//       {
+//         return false;
+//       }
+//     }
+
+//     return true;
+//   }
+
+//   public static void Main(string[] args)
+//   {
+//     Console.WriteLine(IsIsogram("Dermatoglyphics"));
+//     Console.WriteLine(IsIsogram("aba"));
+//     Console.WriteLine(IsIsogram("moOse"));
+//   }
+// }
+
+/*
+Find the Parity Outlier
+
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+Examples
+[2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+*/
+
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
 
 public class Kata
 {
-  public static bool IsIsogram(string str) 
-  {
-    str = str.ToLower();
-    foreach (char c in str)
+    public static int Find(int[] integers)
     {
-      if (str.IndexOf(c) != str.LastIndexOf(c))
+      // loop through first 3 ints too find if the array is mostly odds or mostly evens
+      int odd = 0;
+      int even = 0;
+      for(int i=0; i<3; i++)
       {
-        return false;
+        _ = ((Math.Abs(integers[i] % 2)) == 0) ? even++ : odd++;
       }
+
+      // loop through the entire array and return the number that satisfies the outlier requirement
+      var common = odd > even ? 1 : 0;
+
+      var result = integers.First(x => 
+        !(Math.Abs(x) % 2 == common)
+      );
+
+      return result;
     }
 
-    return true;
-  }
-
-  public static void Main(string[] args)
-  {
-    Console.WriteLine(IsIsogram("Dermatoglyphics"));
-    Console.WriteLine(IsIsogram("aba"));
-    Console.WriteLine(IsIsogram("moOse"));
-  }
+    public static void Main(string[] args)
+    {
+      Console.WriteLine(Find(new int[]{2, 4, 0, 100, 4, 11, 2602, 36}));// 11
+      Console.WriteLine(Find(new int[]{160, 3, 1719, 19, 11, 13, -21}));// 160
+    }
 }
